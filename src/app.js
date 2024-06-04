dfc04988bd7faa847dc07c60ef5d0bab765e6992
jcrews123 <168486450+jcrews123@users.noreply.github.com>
2024-06-04 00:50:13 +0000

    finished project

--- src/app.js
+++ src/app.js
@@ -8,4 +8,26 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  function chooseRandomItem(array) {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+  }
+
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  chooseRandomItem(who);
+  const header = `${chooseRandomItem(who)} ${chooseRandomItem(
+    action
+  )} ${chooseRandomItem(what)} ${chooseRandomItem(when)}`;
+  const excuseh1 = document.querySelector("#excuse");
+  excuseh1.textContent = header;
 };
